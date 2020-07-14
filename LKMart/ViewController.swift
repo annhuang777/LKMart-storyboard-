19b4c816ae40f1bcb5d16cdeeffaff37a592de50
annhuang <ann.huang777@gmail.com>
2020-07-14 23:28:37 +0800

    Initial Commit

---
+++ LKMart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LKMart
+//
+//  Created by Pin yu Huang on 2020/7/14.
+//  Copyright © 2020 Pin yu Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
